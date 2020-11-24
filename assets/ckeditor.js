5d2ac60a8f794df832a6ae4bc8bef6b383b0cc0a
FaaDiiL <fadil.alfarhan@gmail.com>
2020-11-24 15:54:27 +0100

    Freshing up the code a litle bit.

--- assets/ckeditor.js
+++ assets/ckeditor.js
@@ -1,10 +1,19 @@
+//let ed = null
+let ed
+
 DecoupledEditor
-.create(document.querySelector('#editor'))
-.then(editor => {
-     const toolbarContainer = document.querySelector('#toolbar-container');
-     toolbarContainer.appendChild(editor.ui.view.toolbar.element);
-     editor.setData(`<p>${22}</p>`);
-})
-.catch(error => {
-     console.error(error);
-});
+    .create(document.querySelector('#editor'))
+    .then(editor => {
+         ed = editor
+         const toolbarContainer = document.querySelector('#toolbar-container')
+         toolbarContainer.appendChild(editor.ui.view.toolbar.element)
+         //editor.setData(<p>${22}</p>);
+         console.log(ed)
+    })
+    .catch(error => {
+         console.error(error)
+    })
+
+function go(data) {
+     ed.setData(data)
+}
--- assets/index.js
+++ assets/index.js
@@ -4,41 +4,23 @@ let rightDiv = document.querySelector('#right')
 let saveBtn = document.querySelector('#save-btn')
 let loadAll = document.querySelector('#load-btn')
 let noteTitle = document.querySelector('#note-title') //The Note title you want to add
-let ckEditor = document.querySelector('#editor') // The editor
+let editorEl = document.querySelector('#editor') // The editor
 let showAllNotes = document.querySelector('#showAllNotes') // The editor
 let noteContent = document.querySelector('#note-content')
 let noteUl = document.querySelector('#note-list')
 let allTitles = document.querySelector('#allTitles h2')
 let textArea = document.querySelector('#textArea')
 
+// Event listeners
+
+editorEl.addEventListener('click', e => e.target)
+saveBtn.addEventListener('click', saveBtnClicked)
+leftDiv.addEventListener('click', findTheId) // Find the id of the title you click on
+
+// Global Variables
 let notes =[]
 
-// function addNote() {
-//      // Making a loop that push 3 instances of note withe 
-//      for(let i = 0; i<3; i++)
-//      {
-//           let note = {
-//                id: null,
-//                title: '',
-//                note:'',
-//                date:null,
-//                favorit:false,
-//           }
-//           note.id = i
-//           note.title = 'Your title'
-//           note.note = 'This is a note.'
-//           note.date = new Date().toLocaleString('se')
-          
-//           console.log(note.id)
-//           notes.push(note)
-//      }
-//        return note
-// }
-
-// console.log(addNote())
-// console.log(notes)
-
-
+// Functions
 function getNotes() {
      // laddar från localStorage
      let retriveddata = localStorage.getItem('Notes')
@@ -63,10 +45,6 @@ function createNote (title, content) {
      return 
 }
 
-// function findNoteById(noteId){
-   
-// }
-
 function modifieNote(currentNote) {
      let note = {
           id: currentNote.id,
@@ -78,58 +56,21 @@ function modifieNote(currentNote) {
      return note
 }
 
-// Append all notes
-function appendAllNotes(){
-     let myNotesObj = getNotes()
-     myNotesObj.map(note =>{
-          let title = note.title
-          let content = note.content
-          let fullNoteLi = document.createElement('li')
-
-
-          fullNoteLi.innerHTML = `<h2>${title}</h2> <p>${content}</p>`
-          noteUl.appendChild(fullNoteLi)
-          
+function findTheId(e){ 
+     let clickedId = e.target.id
+     getNotes().filter(note => {
+          if(note.id == clickedId)
+          {
+               noteTitle.value = note.title
+               go(note.content)
+          }
      })
 }
 
-
-function showNotes() {
-  getNotes().map(function (note) {
-    let title = document.createElement('h2')
-    let ingredients = document.createElement('p')
-
-    title.innerText = note.title
-    ingredients.innerText = note.content
-
-    showAllNotes.appendChild(title)
-    showAllNotes.appendChild(ingredients)
-  })
-}
-
-// Event listeners
-
-ckEditor.addEventListener('click', (e) =>{
-return e.target
-})
-
-saveBtn.addEventListener('click', (e) =>{
-     console.log('Title: ', noteTitle.value ,'Content: ',ckEditor.innerHTML)
-     createNote(noteTitle.value != ''?noteTitle.value : 'Ingen rubrik!'  ,ckEditor.innerHTML)
+function saveBtnClicked(e){
+     createNote(noteTitle.value != ''?noteTitle.value : 'Ingen rubrik!'  ,editorEl.innerHTML)
      saveNotes()
-})
-
-loadAll.addEventListener('click', (e) =>{
-     noteUl.innerHTML = ''
-appendAllNotes()
-
-
-})
-
-
-
-
-
+}
 
 
 
@@ -138,40 +79,35 @@ window.onload = function(){
      let myNotesObj = getNotes()
      myNotesObj.map(note =>{
 
-          let title = document.createElement('h2')  
+          let title = document.createElement('h5')  
 
           title.innerText = note.title
           title.setAttribute('id', note.id)
            leftDiv.appendChild(title)
-          
      })
 }
 
 
-// create a function that find the id of the matched obj
-function findTheId(e){
-     let clickedId = e.target.id
-     let clicked = e.target
-     
-     getNotes().filter(note => {
-          if(note.id == clickedId)
-          {
-               noteTitle.value = note.title
-               ckEditor.innerHTML =  note.content
 
 
-               console.log(note.content)
-               console.log(note.title)
-          }
-     })
-     
-}
-
-// create a lissener that listen form click on titles
-leftDiv.addEventListener('click', findTheId)
 
 // Check the id of the clicked Title
 
 
 // Find the matched obj...
 
+// Good To Have Stuff
+// // Append all notes
+// function appendAllNotes(){
+//      let myNotesObj = getNotes()
+//      myNotesObj.map(note =>{
+//           let title = note.title
+//           let content = note.content
+//           let fullNoteLi = document.createElement('li')
+
+
+//           fullNoteLi.innerHTML = `<h2>${title}</h2> <p>${content}</p>`
+//           noteUl.appendChild(fullNoteLi)
+          
+//      })
+// }
