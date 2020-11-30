5dffaefa1f73f7bac7a91ea57fb34443f9470a51
FaaDiiL <fadil.alfarhan@gmail.com>
2020-11-30 15:34:07 +0100

    Balloon style inserted modified checkeditor- index.js and index.html.

--- assets/index.js
+++ assets/index.js
@@ -10,19 +10,16 @@ let noteContent = document.querySelector('#note-content')
 let noteUl = document.querySelector('#note-list')
 let allTitles = document.querySelector('#allTitles h2')
 let textArea = document.querySelector('#textArea')
-let infoText = document.querySelector('#info-text')
-
+let OnloadWindow = window
 // Event listeners
-
 editorEl.addEventListener('click', e => e.target)
-saveBtn.addEventListener('click', checkLocalStorage)
+saveBtn.addEventListener('click', saveBtnClicked)
+deleteBtn.addEventListener('click', deleteNote)
 leftDiv.addEventListener('click', findTheId) // Find the id of the title you click on
-deleteBtn.addEventListener('click', deleteNote) // Find the id of the title you click on
-deleteBtn.addEventListener('click', deleteNote) // Find the id of the title you click on
+OnloadWindow.addEventListener('load', loadOnStart )
 
 // Global Variables
-let notes = []
-let visitedFirstTime = false
+let notes =[]
 
 // Functions
 function getNotes() {
@@ -32,11 +29,9 @@ function getNotes() {
      let convertedData = JSON.parse(retriveddata)
      return convertedData
 } 
-
 function saveNotes() {
           localStorage.setItem('Notes', JSON.stringify(notes))
 }
-
 function createNote (title, content) {
      notes.push( {
           id: Date.now(),
@@ -46,23 +41,14 @@ function createNote (title, content) {
           favorite: false,
      })
      saveNotes()
-     document.location.reload(true)
-     
+
 }
+function deleteNote(){
 
-function deleteNote(e) {
-     notes.push(...getNotes())
-     
-     console.log(notes)
-     notes.splice(e.target.value, 1, )
-     console.log(notes)
-
+     notes.splice(deleteBtn.value,1)
      saveNotes()
-      notes = []
-      document.location.reload(true)
+     location.reload();
 }
-
-
 
 function modifieNote(currentNote) {
      let note = {
@@ -74,73 +60,37 @@ function modifieNote(currentNote) {
      }
      return note
 }
-
 function findTheId(e){ 
      let clickedId = e.target.id
-     getNotes().filter((note,i)=> {
+     
+     getNotes().filter((note, index) => {
           if(note.id == clickedId)
           {
                noteTitle.value = note.title
-               go(note.content)
-               deleteBtn.value=i 
-               infoText.innerHTML = (`<h4> ${moment().locale('sv').from(note.id)}</h4>`)     
+               deleteBtn.setAttribute('value', index ) 
+               console.log(deleteBtn)
+               editor.setData(note.content)
+               
           }
      })
 }
-
 function saveBtnClicked(e){
-     createNote(noteTitle.value != ''?noteTitle.value : 'Ingen rubrik!'  ,editorEl.innerHTML)
+     createNote(noteTitle.value != ''?noteTitle.value : 'Ingen rubrik!', editorEl.innerHTML)
      saveNotes()
-     
-
+     location.reload();
 }
-
-function checkLocalStorage() {
-  if (localStorage.getItem('Notes')) {
-    notes.push(...getNotes())
-  }
-  saveBtnClicked()
-  notes = []
-}
-
-
-// Show all titles in the left  div (, )
-window.onload = function(){
-     if(localStorage.getItem('infoBox'))
-     {
-          visitedFirstTime = true
-         
-     }else {
-          
-
-infoBox.innerHTML = `
-<div class="flexContainer" id="container">
-               <div class="containerItems">
-                    <h2 id="welcome">Let's take some notes!</h2>
-                    <p class="instructions" id="instructionsTitle">
-                         Thanks for choosing to use Quire! <br> Here's some basic information to get you started:</p>
-                    <ul class="instructions" id="instructionsList">
-                         <li>To make a new note press the "+" symbol at the bottom right.</li>
-                         <li> To search through your notes press the looking glas at the top right.</li>
-                         <li>Click the cog on the right hand side to access the settings.</li>
-                    </ul>
-               </div>
-               <button id="exit" onclick="closeInfo()">Let's get started!</button>
-          </div>
-
-          <div id="overlay"></div>
-`
+function loadOnStart(){
+     if(getNotes()){
+          notes = getNotes()
+          let myNotesObj = getNotes()
+          myNotesObj.map(note =>{
+               let title = document.createElement('h5')  
+               title.innerText = note.title
+               title.setAttribute('id', note.id)
+               leftDiv.appendChild(title)
+          })  
      }
      
-     let myNotesObj = getNotes()
-          myNotesObj.map(note =>{
-          let title = document.createElement('h5')  
-
-          title.innerText = note.title
-          title.setAttribute('id', note.id)
-          leftDiv.appendChild(title)
-
-     })
 }
 
 
@@ -167,29 +117,3 @@ infoBox.innerHTML = `
           
 //      })
 // }
-
-/*---This block is for the close button on the popup/info overlay---*/ 
-//objects 
-let container = document.getElementById("container");
-let overlay = document.getElementById("overlay");
-let infoBox = document.getElementById("info");
-
-
-//Closes the div with information
-function closeInfo() {
-     
-     localStorage.setItem('infoBox', visitedFirstTime)
-    
-     if (container.style.display === "none") {
-      container.style.display = "block";
-    } else {
-      container.style.display = "none";
-    }
-
-  if (overlay.style.display === "none") {
-    overlay.style.display = "block";
-  } else {
-    overlay.style.display = "none";
-  }
-  visitedFirstTime = true
-} 
---
+++ assets/myCkeditor.js
@@ -0,0 +1,9 @@
+BalloonEditor
+    .create( document.querySelector( '#editor' ) )
+    .then( newEditor => {
+        editor = newEditor
+        
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
