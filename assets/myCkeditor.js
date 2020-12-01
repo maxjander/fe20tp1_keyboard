20517e1b4aa9c164b07e72123545ee7bd0870264
FaaDiiL <fadil.alfarhan@gmail.com>
2020-12-01 01:13:05 +0100

    myCkeditor.js modified

--- assets/myCkeditor.js
+++ assets/myCkeditor.js
@@ -2,7 +2,6 @@ BalloonEditor
     .create( document.querySelector( '#editor' ) )
     .then( newEditor => {
         editor = newEditor
-        
     } )
     .catch( error => {
         console.error( error );
