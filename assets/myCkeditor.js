50d27b07c4967679d84a0b2a2bf371f5a4cfbf4c
Max Jander <maxjander@gmail.com>
2020-12-03 09:21:10 +0100

    Fixed toolbar config

--- assets/myCkeditor.js
+++ assets/myCkeditor.js
@@ -1,5 +1,17 @@
 BalloonEditor
-    .create( document.querySelector( '#editor' ) )
+    .create( document.querySelector( '#editor' ), {
+      toolbar: {
+           items: [  'heading', '|','bold', 'italic', '|', 'comment', 'bulletedList', 'numberedList', ],
+
+           heading: {
+          options: [
+              { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+              { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+              { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+          ]
+      }
+       }
+    } )
     .then( editor => {
         window.editor = editor;
     } )
